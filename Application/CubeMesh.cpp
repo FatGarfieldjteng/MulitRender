1920a1bb06bc5804ba6be3bbb71657a221f07d8b
tengj <jun.teng@autodesk.com>
2022-11-22 09:39:52 +0800

    Add CubeMesh class as child class of Mesh

---
+++ Application/CubeMesh.cpp
@@ -0,0 +1,13 @@
+#include "framework.h"
+#include "CubeMesh.h"
+#include "helper.h"
+
+CubeMesh::CubeMesh()
+{
+
+}
+
+CubeMesh::~CubeMesh()
+{
+
+}
---
+++ Application/CubeMesh.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Mesh.h"
+#include <d3d12.h>
+
+class CubeMesh : public Mesh
+{
+public:
+
+    CubeMesh();
+
+    ~CubeMesh();
+};
