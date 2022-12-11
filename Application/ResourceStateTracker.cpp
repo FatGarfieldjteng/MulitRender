b1a2e3f5091d6090cdd09864c5a016e1881a9556
FatGarfieldjteng <fatgarfieldjteng@hotmail.com>
2022-12-11 18:01:57 +0800

    Add ResourceStateTracker class for resource state management

---
+++ Application/ResourceStateTracker.cpp
@@ -0,0 +1,12 @@
+#include "framework.h"
+#include "ResourceStateTracker.h"
+
+ResourceStateTracker::ResourceStateTracker()
+{
+
+}
+
+ResourceStateTracker::~ResourceStateTracker()
+{
+
+}
---
+++ Application/ResourceStateTracker.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <d3d12.h>
+#include <mutex>
+
+class ResourceStateTracker
+{
+public:
+
+    ResourceStateTracker();
+
+    ~ResourceStateTracker();
+};
